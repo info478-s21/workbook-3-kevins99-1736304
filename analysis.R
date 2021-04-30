ce880e037ad258c5e7c09cb4508b9d82e3180e72
kevinsong99 <zikaisong123@gmail.com>
2021-04-30 12:02:38 +0800

    finish assignment

--- analysis.R
+++ analysis.R
@@ -6,17 +6,28 @@ library(dplyr)
 library(ggplot2)
 
 # Create the `charts/` directory (you can do this from R!)
+dir.create("charts", showWarnings = FALSE)
 
 # Load prepped data
+health <- read.csv("./data/prepped/all_data.csv")
 
 # Are HALE and life expectancy correlated?
 # - Plot 2016 life expectancy against 2016 HALE. Save the graph to `charts/`
 # - Compute the correlation between 2016 life expectancy against 2016 HALE
+data2016 <- health%>%
+  filter(year == 2016)
+ggplot(data2016) + geom_point(mapping = aes(x = le, y = hale)) +
+  labs(title = "Life Expectancy vs HALE", x = "Life Expectancy", y = "HALE")
+ggsave("charts/le_hale_graph.png")
+cor(data2016$hale, data2016$le)
 
 # Are HALE and DALYs correlated?
 # - Plot 2016 HALE against 2016 DALYs. Save the graph to `charts/`
 # - Compute the correlation between 2016 HALE and DALYs
-
+ggplot(data2016) + geom_point(mapping = aes(x = dalys, y = hale)) +
+  labs(title = "DALYS vs HALE", x = "DALY", y = "HALE")
+ggsave("charts/dalys_hale_graph.png")
+cor(data2016$hale, data2016$dalys)
 
 # As people live longer, do they live healthier lives 
 # (i.e., is a smaller fraction of life spent in poor health)?
@@ -26,11 +37,20 @@ library(ggplot2)
 # This will create `hale_2016`, `hale_1990`, `le_2016`, etc.
 # To do this, I suggest that you use the `pivot` function in the new
 # tidyverse release:https://tidyr.tidyverse.org/articles/pivot.html#wider
-
+wide <- health%>%
+  pivot_wider(names_from = year, values_from = c(hale, le, dalys))
 
 # Create columns to store the change in life expectancy, and change in hale
-
+wide <- wide%>%
+  mutate(hale_diff = hale_2016 - hale_1990,
+         le_diff = le_2016 - le_1990)
 
 # Plot the *change in hale* against the *change in life expectancy*
 # Add a 45 degree line (i.e., where x = y), and save the graph to `charts/`
 # What does this mean?!?! Put your interpretation below
+ggplot(wide) + geom_point(mapping = aes(x = le_diff, y = hale_diff)) +
+  labs(title = "Life Expectancy difference vs HALE difference", x = "Change in Life Expectancy", y = "Change in HALE") +
+  geom_abline(intercept = 0, slope = 1) + xlim(-15, 20) + ylim(-15, 20)
+ggsave("charts/change_graph.png")
+# Interpretation: The dots correspond with the 45 degree line indicates that the change in life expectancy and
+# the change in HALE have similar amount for the same country throughout the given time length.
